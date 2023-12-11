ecd6d1db627f0387bb4c7022245bab2362f698d3
Niraj Kumar <nirajk23523@gmail.com>
2023-12-11 18:04:47 +0530

    Dev : added db connection

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+// we are in development mode, so we can safely assign to globalThis.prisma
+// to support hot-reloading
